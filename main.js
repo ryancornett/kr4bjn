dce1e6ae1b5ee6baedd1edb0e078ca6695337947
Ryan Cornett <ryandcornett@gmail.com>
2025-03-03 16:35:25 -0500

    Add solar-terrestrial data from hamqsl.com

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ const verse = bibleVerses[Math.floor(Math.random() * bibleVerses.length)];
 verseText.textContent = verse.text;
 verseRef.textContent = `${verse.book} ${verse.chapter}:${verse.verses} BSB`;
 
+const year = new Date().getFullYear();
+let rights = document.getElementById('rights');
+rights.textContent = year == 2025 ? "All rights reserved 2025" : `All rights reserved 2025-${year}`;
